b11d5092cd9033c4b888536fbc05be10972904a9
AndreevIlya <katauri@yandex.ru>
2019-11-27 21:49:35 +0300

    Remaining Realm methods implemented.

--- app/src/main/java/ru/shuffleattractors/data/databases/CommandRealm.kt
+++ app/src/main/java/ru/shuffleattractors/data/databases/CommandRealm.kt
@@ -9,13 +9,26 @@ import ru.shuffleattractors.domain.entities.CommandEntity
 open class CommandRealm(): RealmModel {
     var iterationsCount: Int = 0
     var countModified: Int = 0
-    var computerType: String? = null
-    var modifiedArrayItems: String? = null
+    lateinit var computerType: String
+    lateinit var modifiedArrayItems: String
 
-    constructor(entity: CommandEntity): this(){
+    private constructor(entity: CommandEntity): this(){
         iterationsCount = entity.iterationsCount
         countModified = entity.countModified
         computerType = entity.computerType
         modifiedArrayItems = Gson().toJson(entity.modifiedArrayItems)
+    }
+
+    companion object{
+        fun convertToRealm(entity: CommandEntity): CommandRealm{
+            return CommandRealm(entity)
+        }
+        fun convertFromRealm(command: CommandRealm): CommandEntity{
+            return CommandEntity(
+                command.iterationsCount,
+                command.countModified,
+                command.computerType,
+                Gson().fromJson(command.modifiedArrayItems,Array<Int>::class.java))
+        }
     }
 }
--- app/src/main/java/ru/shuffleattractors/data/databases/ResultRealm.kt
+++ app/src/main/java/ru/shuffleattractors/data/databases/ResultRealm.kt
@@ -3,6 +3,7 @@ package ru.shuffleattractors.data.databases
 import com.google.gson.Gson
 import io.realm.RealmModel
 import io.realm.annotations.RealmClass
+import ru.shuffleattractors.domain.entities.CommandEntity
 import ru.shuffleattractors.domain.entities.ResultEntity
 
 @RealmClass
@@ -11,12 +12,19 @@ open class ResultRealm() : RealmModel {
     private lateinit var command: CommandRealm
     private lateinit var result: String
 
-    constructor(command: CommandRealm, result: String): this(){
+    private constructor(command: CommandRealm, result: String): this(){
         this.command = command
         this.result = result
     }
 
-    fun getResult(): ResultEntity{
+    fun getResultEntity(): ResultEntity{
         return ResultEntity(Gson().fromJson(result,Array<Int>::class.java))
+    }
+
+    companion object{
+        fun convertToRealm(command: CommandRealm,
+                           resultEntity: ResultEntity): ResultRealm{
+            return ResultRealm(command,Gson().toJson(resultEntity))
+        }
     }
 }
